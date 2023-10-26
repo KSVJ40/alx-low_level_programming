a958bd533c2c5c0f456f8fc1fcec234dfd82ad68
KSVJ40 <valentinesonna@gmail.com>
2023-10-26 16:36:13 -0700

    task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: the string is printed
+ * Rettun: always 0 (success)
+ */
+void _print_rev_recursion(char *s)
+{
+	if(*s)
+	{
+		_putchar(*s);
+		_print_rev_recursion(s + 1);
+	}
+}
